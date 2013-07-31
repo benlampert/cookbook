1bb728932c7a55dc5ec020391c8305da9a860b1e
blamps <benlampert@gmail.com>
2013-07-30 21:21:38 -0400

    adding in the variables

---
+++ a.py
@@ -0,0 +1,52 @@
+a = [ {'x':1, 'y':2}, {'x':1, 'y':3}, {'x':1, 'y':2}, {'x':2, 'y':4}]
+
+items = [0,1,2,3,4,5,6]
+
+
+words = [
+'look', 'into', 'my', 'eyes', 'look', 'into','my', 'eyes', 'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'the', 'eyes','not', 'around', 'the', 'eyes', "don't", 'look', 'around', 'the', 'eyes','look', 'into', 'my', 'eyes', "you're", 'under'
+]
+
+rows2 = [
+{'fname': 'Brian', 'lname':'Jones', 'uid':1003},
+{'fname': 'David', 'lname':'Beazley', 'uid':1002},
+{'fname': 'John', 'lname':'Cleese', 'uid':1001},
+{'fname': 'Big', 'lname': 'Jones', 'uid':1004}
+]
+
+rows = [
+{'address': '5412 N CLARK', 'date': '07/01/2012'},
+{'address': '5148 N CLARK', 'date': '07/04/2012'},
+{'address': '5800 E 58TH', 'date': '07/02/2012'},
+{'address': '2122 N CLARK', 'date': '07/03/2012'},
+{'address': '5645 N RAVENSWOOD', 'date':'07/02/2012'},
+{'address': '1060 W ADDISON', 'date': '07/02/2012'},
+{'address': '4801 N BROADWAY', 'date': '07/01/2012'},
+{'address': '1039 W GRANVILLE', 'date': '07/04/2012'},
+]
+
+mylist = [1, 4, -5, 10, -7, 2, 3, -1]
+values = ['1', '2', '-3', '-', '4', 'N/A', '5']
+
+addresses = [
+'5412 N CLARK',
+'5148 N CLARK',
+'5800 E 58TH',
+'2122 N CLARK',
+'5645 N RAVENSWOOD',
+'1060 W ADDISON',
+'4801 N BROADWAY',
+'1039 W GRANVILLE',
+]
+
+counts = [0, 3, 10, 4, 1, 7, 6, 1]
+
+prices = {
+'ACME': 45.23,
+'AAPL': 612.78,
+'IBM': 205.55,
+'HPQ': 37.20,
+'FB': 10.75,
+}
+
+
